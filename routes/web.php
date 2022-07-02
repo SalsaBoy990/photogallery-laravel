dc20a2c5ba8fa2e4460c5850dc36969e265aa126
Gulácsi András <guland@protonmail.com>
2022-07-02 03:16:33 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\FileAccessController;
 use App\Http\Controllers\GoalController;
 use App\Http\Controllers\LocalizationController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,14 +37,17 @@ Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'app'],
         Route::get('gallery/create', [GalleryController::class, 'create'])->name('gallery.create');
         Route::get('gallery/{gallery}', [GalleryController::class, 'show'])->name('gallery.show');
         
-
         /* Photos */
         Route::get('photo/{photo}', [PhotoController::class, 'show'])->name('photo.show');
         Route::post('photo', [PhotoController::class, 'store'])->name('photo.store');
         Route::get('photo/create/{gallery}', [PhotoController::class, 'create'])->name('photo.create');
         Route::put('photo/update/{photo}', [PhotoController::class, 'update'])->name('photo.update');
 
+        // Goals (bucketlist)
         Route::resource('goal', GoalController::class);
+
+        // Tags used for galleries and photos
+        Route::resource('tag', TagController::class);
     }
 );
 
